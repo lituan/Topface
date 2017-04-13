d9419a6304f80c773d5caa6461e73692223298bc
lituan <imlituan@gmail.com>
2017-04-13 14:37:32 +0800

    fname

--- get_seq_from_wdsp.py
+++ get_seq_from_wdsp.py
@@ -5,11 +5,12 @@ usage: python get_seq_from_wdsp.py wdsp_f
 import sys
 from wdsp import Wdsp
 
+fname = os.path.split(sys.argv[-1])[1].split('.')[0]
 with open(sys.argv[-1]) as o_f:
     wdsp = Wdsp(o_f)
     seqs = wdsp.seqs
 
-    with open('getted_seq.fa', 'w') as w_f:
+    with open(fname+'_seq.fa', 'w') as w_f:
         for pro, seq in seqs.iteritems():
             print >> w_f, '>', pro
             for s in [seq[i:i + 80] for i in range(0, len(seq), 80)]:
